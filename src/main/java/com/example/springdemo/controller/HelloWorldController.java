382f73dd10912a856d1ad2442d1485e6e466ae58
wanmeiyu <wanmeiyu@meituan.com>
2025-02-28 16:58:52 +0800

    Hello world api

--- src/main/java/com/example/springdemo/controller/HelloWorldController.java
+++ src/main/java/com/example/springdemo/controller/HelloWorldController.java
@@ -1,13 +1,15 @@
 package com.example.springdemo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: wanmeiyu
  * @Date: 2025/2/28
  **/
-@Controller
+@RestController
+@RequestMapping("/")
 public class HelloWorldController {
     @GetMapping("/hello")
     public String hello() {
